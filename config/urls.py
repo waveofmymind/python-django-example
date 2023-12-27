eb0a14663cde8e57e888ceebfb8acfae6b1bc62b
waveofmymind <sjun0913@gmail.com>
2023-12-27 14:37:35 +0900

    feat: config 앱을 초기화한다.

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from config.views import main
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main)
 ]
